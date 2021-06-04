d8e546efcfd6ab1fb39c15486afd12b7b660a59e
Diana-russ <84708817+Diana-russ@users.noreply.github.com>
2021-06-04 14:26:25 +0300

    BEM rework
    Переделала проект под технологию BEM

--- Diana/JS/Js.js
+++ Diana/JS/Js.js
@@ -1,9 +1,8 @@
-
 var urims;
 sendbutton.onclick = image_display;
 selecting_faile.onchange = readFile;
 function image_display(){
-	alert(input_email.value + "\n" + input_text_fam.value + "\n" + input_text_name.value + "\n" + input_text_othestvo.value+"\n"+textarea.value);
+	alert(input_email.value + "\n" + input_text_fam.value + "\n" + input_text_name.value + "\n" + input_text_othestvo.value+"\n"+style_textarea.value);
 	var im = document.getElementById("imges");
 	im.src = urims;
 };
@@ -11,4 +10,3 @@ function readFile() {
   let file = selecting_faile.files[0];
 	urims = URL.createObjectURL(file);
 }
-
